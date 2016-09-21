8922f32615e8498d997b9ca818091eb033827742
Josimar <josimarlochcm@gmail.com>
2016-09-21 16:03:12 -0300

    Remoção Linha comentadas

--- badge/src/br/net/loch/badge/dao/HibernateConfiguration.java
+++ badge/src/br/net/loch/badge/dao/HibernateConfiguration.java
@@ -26,14 +26,7 @@ import org.sqlite.JDBC;
  * @author josimar
  */
 public class HibernateConfiguration {
-    public static final String userName = "root";
-    public static final String password = "grupoaion";
-    public static final String dataBase = "scp";
-    
-    // configuracões somente uma vez 
-    // build  valida somente uma vez
-    // build  valida somente uma vez
-    // Session uma sessão para cada transação ou um conjunto de transações
+
 
     private static Configuration cfg = null;
     private static SessionFactory factory = null;
@@ -41,17 +34,8 @@ public class HibernateConfiguration {
     public static SessionFactory getSessionFactory() {
         if (cfg == null) {
             cfg = new Configuration();
-       /*      <property name="hibernate.hbm2ddl.auto" value="update"/>
-      <property name="hibernate.show_sql" value="true"/>
-      <property name="hibernate.format_sql" value="true"/>
-      <property name="javax.persistence.jdbc.driver" value="org.sqlite.JDBC"/>
-      <property name="javax.persistence.jdbc.url" value="jdbc:sqlite:badge.db"/>
-      <property name="hibernate.dialect" value="br.net.loch.badge.dao.SQLiteDialect"/>
-      <property name="javax.persistence.schema-generation.database.action" value="none"/> */
+   
             cfg.setProperty("hibernate.connection.driver", "org.sqlite.JDBC");
-          //  cfg.setProperty("hibernate.connection.username", userName);
-          //  cfg.setProperty("hibernate.connection.password", password);
-            //cfg.setProperty("hibernate.connection.url", "jdbc:mysql://ec2-23-21-211-172.compute-1.amazonaws.com:3306/"+dataBase);
             cfg.setProperty("hibernate.connection.url", "jdbc:sqlite:badge.db");
             cfg.setProperty("hibernate.show_sql", "true");
             cfg.setProperty("hibernate.format_sql", "true");
@@ -68,7 +52,6 @@ public class HibernateConfiguration {
         }
         return factory;
     }
-    //podemos setar o autocomit como true e nao precisa usar begi tansaction
 
     public static void createSchema() {
         getSessionFactory().close();
