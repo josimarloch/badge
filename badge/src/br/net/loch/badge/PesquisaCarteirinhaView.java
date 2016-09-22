9e0f13951e070992ea8adb527afa302007e876de
josimar <josimarlochcm@gmail.com>
2016-09-22 00:47:35 -0300

    Criar tela de pesquisa de carteirinhas #11

---
+++ badge/src/br/net/loch/badge/PesquisaCarteirinhaView.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.net.loch.badge;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author josimar
+ */
+public class PesquisaCarteirinhaView extends Application{
+
+   
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void start(Stage primaryStage)  {
+        URL url = getClass().getResource("PesquisaCarteirinha.fxml");
+        System.out.println(url.getFile());
+        Parent fxmlParent;
+        try {
+            fxmlParent = (Parent) FXMLLoader.load(url);
+        primaryStage.setScene(new Scene(fxmlParent,600,400));
+        primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(PesquisaCarteirinhaView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
