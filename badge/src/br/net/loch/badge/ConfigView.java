100e7b7fc6a70b199b7dd3c9f41499c615d1f88c
josimar <josimarlochcm@gmail.com>
2016-09-28 18:21:23 -0300

    Tela de configuração de Db Remoto #20

---
+++ badge/src/br/net/loch/badge/ConfigView.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.net.loch.badge;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author josimar
+ */
+public class ConfigView extends Application {
+    
+   
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void start(Stage primaryStage)  {
+        URL url = getClass().getResource("Config.fxml");
+        System.out.println(url.getFile());
+        Parent fxmlParent;
+        try {
+            fxmlParent = (Parent) FXMLLoader.load(url);
+        primaryStage.setScene(new Scene(fxmlParent,660,440));
+        primaryStage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(PesquisaCarteirinhaView.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
